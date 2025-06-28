849148235e86b0bec0c2c25b11631cfd8a749827
vinay-akvinay-ak <23481a0488@gmail.com>
2025-06-28 09:20:53 +0530

    Delete document/project files/Project_files directory

--- document/project files/Project_files/app/api/dashboard_router.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter(
-    prefix="/dashboard",
-    tags=["Dashboard"]
-)
-
-# Endpoints for aggregated dashboard data will be added here.
-# For example: GET /kpi-summary 
